e9f0b077abdf7d1a8418658ac2c9ee8812195f56
Kevin Araújo <kfa_34@hotmail.com>
2022-02-17 14:44:32 -0300

    Finished backend.

--- controllers/UserController.ts
+++ controllers/UserController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import EmailService from '../services/EmailService';
 
 const users = [
     { name: 'Kevin', email: 'kevin@gmail.com'},
@@ -7,5 +8,15 @@ const users = [
 export default {
     async index(req: Request, res: Response) {
         return res.json(users);
+    },
+
+    async create(req: Request, res: Response) {
+        const emailService = new EmailService();
+        emailService.sendMail({
+            to: { name: 'Kevin', email: 'kevin@gmail.com'},
+            message: { subject: 'Teste', body: 'Corpo do email' }
+        });
+
+        res.send();
     }
 }
--- readme.md
+++ readme.md
@@ -1,4 +1,5 @@
 # Masterclass-typescript
 
-One fast-forward course teaching about typescript.
+This is a fast-forward course teaching about typescript.
+
 To execute the project run on terminal `yarn dev:server`
